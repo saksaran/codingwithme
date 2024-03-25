25c14341e45e0d7d5c4da8d92018b1a8b7b0b696
damn <110712863+saksaran@users.noreply.github.com>
2024-03-25 21:58:15 +0700

    Add files via upload

--- src/router/index.js
+++ src/router/index.js
@@ -4,40 +4,35 @@ import RegisterView from '@/views/RegisterView.vue'
 import LoginView from '@/views/LoginView.vue'
 import MovieDes from '@/components/descriptionpage/MovieDescriptionPage.vue'
 import NotFound from '@/views/NotFound.vue'
-import CommentedPage from '@/components/commentedpage/commentedPage.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Homepage,
-    },
-    {
-      path: '/register',
-      name: 'register',
-      component: RegisterView,
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: LoginView,
-    },
-    {
-      path: '/movie/:id',
-      component: MovieDes,
-    },
-    {
-      path: '/:notfound(.*)',
-      component: NotFound,
-    },
-    {
-      path: '/commented',
-      name: 'commented',
-      component: CommentedPage,
-    },
-  ],
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: Homepage
+        },
+        {
+            path: '/register',
+            name: 'register',
+            component: RegisterView
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: LoginView
+        },
+        {
+            path: '/movie/:id',
+            component: MovieDes,
+        },
+        {
+            path: '/:notfound(.*)',
+            component: NotFound
+        }
+    ]
+
 })
 
 export default router
